553dfeebb5884fe09f866f528cb28388f212c237
thomasrocas <thomasrocas@anxlife.com>
2025-09-22 18:08:48 -0700

    Update field-options.js

--- shared/field-options.js
+++ shared/field-options.js
@@ -1,29 +1,56 @@
 export const ORGANIZATION_OPTIONS = Object.freeze([
-  'People Ops',
-  'Corporate',
-  'Retail',
+  'Home Health',
+  'Hospice',
+  'HH / HO',
+  'Palliative (T.I.M.E.)',
+  'Ligare',
 ]);
 
 export const SUB_UNIT_OPTIONS = Object.freeze([
-  'New Hires',
-  'Leadership',
-  'Store Operations',
+  'San Francisco',
+  'San Mateo',
+  'San Jose',
+  'EastBay',
 ]);
 
 export const DISCIPLINE_TYPE_OPTIONS = Object.freeze([
-  'Technical',
-  'Customer Service',
-  'Operations',
+  'RN',
+  'LVN',
+  'PT',
+  'HHA',
+  'MSW',
+  'PTA',
+  'Spiritual Counselor',
+  'ST',
+  'OT',
+  'OTA',
+  'Admin',
+  'MD',
+  'NP',
+  'Music Therapist',
+  'AE',
+  'PCA',
 ]);
 
 export const DELIVERY_TYPE_OPTIONS = Object.freeze([
-  'In person',
+  'In-Person',
   'Virtual',
-  'Self-paced',
+  'Hybrid',
+  'Self-Directed',
+  'On-the-Job',
 ]);
 
 export const DEPARTMENT_OPTIONS = Object.freeze([
-  'Engineering',
-  'Retail',
-  'People Ops',
+  'Intake',
+  'Rehab',
+  'Nursing',
+  'Social Work',
+  'Bereavement',
+  'Administrative',
+  'Supplies',
+  'Medical Record',
+  'IT',
+  'Utilization and Management',
+  'Revenue Cycle Management',
+  'Wound Care',
 ]);
